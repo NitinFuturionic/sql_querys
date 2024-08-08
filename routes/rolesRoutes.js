8c1064587bdad6dd667baaffa5f9a7607090f3e0
NitinFuturionic <nitin@futurionic.com>
2024-08-08 20:38:58 +0530

    Delete routes directory

--- routes/rolesRoutes.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router = express.Router();
-const rolesController=require('../controllers/rolesController');
-
-
-router.post('/api/roles',rolesController.insertRolesData )
-router.get('/api/roles',rolesController.getRoles)
-router.put('/api/roles',rolesController.updateData)
-module.exports =router;
--- routes/userGroupRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router = express.Router();
-const userGroupController=require('../controllers/userGroupController')
-
-
-router.post("/api/usergroup",userGroupController.insertUsergroupData )
-router.get("/api/usergroup",userGroupController.getUsergroup)
-module.exports =router;
